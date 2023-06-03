1230315ac82778ceb38db031ef43e9575c9cec79
Spyrosgg <spyrosgg@protonmail.com>
2023-06-03 18:40:25 +0300

    added workflow to deply gh-pages. Also changed to public

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
+  integrations: [tailwind(), image(), react()],
+  site: 'https://spyrosgg.github.io',
+  base: '/design-engineering',
 });
