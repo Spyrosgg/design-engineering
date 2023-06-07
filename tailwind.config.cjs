29ffb40bdbdad181e3ac7cc8a6ac02c5c78ab1db
Spyrosgg <spyrosgg@protonmail.com>
2023-06-07 23:52:49 +0300

    changed Images to img /public/assets. Some more additions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
     },
     plugins: [
