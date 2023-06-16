600a81545f0c0849fd47ca750b5d18f4d541076d
Spyrosgg <spyrosgg@protonmail.com>
2023-06-16 23:43:10 +0300

    logo

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 // export const SITE_TITLE = 'Desing + Engineering';
-export const SITE_TITLE = 'Diagrid =';
-export const SITE_TITLE_SMALL = 'D =';
+export const SITE_TITLE = 'Diagrid';
+export const SITE_TITLE_SMALL = 'DDE';
 // export const SITE_TITLE_SMALL = 'D+E';
 
 export const SITE_DESCRIPTION = 'Welcome to our website!';
