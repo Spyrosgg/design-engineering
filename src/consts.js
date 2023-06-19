c8dbc12181fe776ccf9f2be195cce13defb42870
Spyrosgg <spyrosgg@protonmail.com>
2023-06-19 20:16:57 +0300

    updated cont for email and tel

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,9 @@
 // export const SITE_TITLE = 'Desing + Engineering';
 export const SITE_TITLE = "Diagrid";
 export const SITE_TITLE_SMALL = "DDE";
+export const EMAIL = "diagrid.com";
+export const TEL_UK = "0777777777";
+export const TEL_GR = "6900000000";
 // export const SITE_TITLE_SMALL = 'D+E';
 
 export const SITE_DESCRIPTION = "Welcome to our website!";
