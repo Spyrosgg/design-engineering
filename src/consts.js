377534ff219b7e8ad99b40469865c1f0dcae6720
Spyrosgg <spyrosgg@protonmail.com>
2023-06-23 18:01:19 +0300

    changed to vantagede.com

--- src/consts.js
+++ src/consts.js
@@ -2,8 +2,8 @@
 export const SITE_TITLE = "Vantage";
 export const SITE_TITLE_SMALL = "VDE";
 export const EMAIL = "vantagede.com";
-export const TEL_UK = "0777777777";
-export const TEL_GR = "6900000000";
+export const TEL_UK = "7491963814";
+export const TEL_GR = "6986689406";
 // export const SITE_TITLE_SMALL = 'D+E';
 
 export const SITE_DESCRIPTION = "Welcome to our website!";
