75fc7c9b7b40714e3ec624638c4d83e1faf8b664
Spyrosgg <spyrosgg@protonmail.com>
2023-07-29 10:11:59 +0100

    changing back to vde

--- src/consts.js
+++ src/consts.js
@@ -1,7 +1,7 @@
-export const SITE_SUB_TITLE = "Structural Engineering";
-export const SITE_TITLE = "P+G Structural Engineering";
-export const SITE_TITLE_HEADER = "P+G";
-export const SITE_TITLE_SMALL = "P+G";
+export const SITE_SUB_TITLE = "Design Engineering";
+export const SITE_TITLE = "Vantage Design Engineering";
+export const SITE_TITLE_HEADER = "Vantage";
+export const SITE_TITLE_SMALL = "VDE";
 export const EMAIL = "pgse.co.uk";
 export const TEL_UK = "7599356987";
 export const TEL_GR = "6986689406";
